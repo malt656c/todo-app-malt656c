034a1a76234d94209fd426f9637b16c1eff34b3f
Malte Jensen <malt656c@stud.kea.dk>
2023-09-13 13:13:30 +0200

    update?

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 import { newTask, updateCounter } from "./scripts/newTask.js";
 import { onRefresh } from "./scripts/onRefresh.js";
 import { colorPicker } from "./scripts/interactivity.js";
-import { addToLocalStorage} from "./scripts/localStorage.js";
+import { addToLocalStorage,updateLocalStorage} from "./scripts/localStorage.js";
 
 const addTaskBtn = document.querySelector("#newTaskBtn");
 addTaskBtn.addEventListener("click", addNewTask);
--- scripts/newTask.js
+++ scripts/newTask.js
@@ -1,4 +1,4 @@
-import { updateLocalStorage, deleteLocalStorage } from "./localStorage.js";
+import { updateLocalStorage, deleteLocalStorage, addToLocalStorage } from "./localStorage.js";
 export function newTask(task) {
   /* diffinition og kloning af template*/
   let taskTemplate = document.querySelector("#taskTemplate").content;
